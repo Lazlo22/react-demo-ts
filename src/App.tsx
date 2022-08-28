d1d9341a904c50208372714b30333246ddb9fcc4
Lazlo22 <v.prihodski@gmail.com>
2022-08-28 16:40:05 +0300

    implemented Button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
+import Button from "./components/Button/Button";
+
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <Button />
     </div>
   );
 }
